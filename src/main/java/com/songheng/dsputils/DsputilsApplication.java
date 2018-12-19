53880b72a8fdab04c444f609f29531d445d97af0
Zhang Shuai <zhangshuai@021.com>
2018-12-19 12:18:23 +0800

    初始化项目

---
+++ src/main/java/com/songheng/dsputils/DsputilsApplication.java
@@ -0,0 +1,14 @@
+package com.songheng.dsputils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsputilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsputilsApplication.class, args);
+	}
+
+}
+
